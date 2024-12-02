355d94b000d3ae19e4a3460dfa45213be29808ee
Rud Faden <rudfaden@gmail.com>
2024-12-02 13:17:25 +0100

    day2 formatted

--- 2024/day2/day2.py
+++ 2024/day2/day2.py
@@ -3,17 +3,21 @@ def order(numbers: list) -> bool:
         return True
 
     # Determine if the sequence is increasing or decreasing
-    increasing = all(numbers[i] < numbers[i + 1] for i in range(len(numbers) - 1))
-    decreasing = all(numbers[i] > numbers[i + 1] for i in range(len(numbers) - 1))
+    increasing = all(numbers[i] < numbers[i + 1]
+                     for i in range(len(numbers) - 1))
+    decreasing = all(numbers[i] > numbers[i + 1]
+                     for i in range(len(numbers) - 1))
 
     return increasing or decreasing
 
+
 def distinance(numbers: list) -> bool:
-    dist=True
+    dist = True
     for i in range(1, len(numbers)):
-        if not(0 < abs(numbers[i] - numbers[i-1]) < 4):
-            dist=False
+        if not (0 < abs(numbers[i] - numbers[i-1]) < 4):
+            dist = False
     return dist
+
 
 def dampner(numbers):
     for i in range(len(numbers)):
@@ -23,19 +27,20 @@ def dampner(numbers):
             return True
     return False
 
+
 def is_safe(numbers: list) -> bool:
     return order(numbers) and distinance(numbers)
-    
+
+
 safe = 0
-dampend_safe=0
+dampend_safe = 0
 with open('2024/day2/input.txt', 'r') as file:
     for line in file:
         l = list(map(int, line.split()))
         if is_safe(l):
             safe += 1
         elif dampner(l):
-            dampend_safe+=1
-            
+            dampend_safe += 1
 
-print(safe,dampend_safe, safe+dampend_safe)
-    
+
+print(safe, dampend_safe, safe+dampend_safe)
