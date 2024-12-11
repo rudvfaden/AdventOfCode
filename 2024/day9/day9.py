31553498cd5dc090f7be53f1cfd640127f5101ee
Rud Faden <rudfaden@gmail.com>
2024-12-11 21:33:30 +0100

    a lot faster

--- 2024/day9/day9.py
+++ 2024/day9/day9.py
@@ -1,42 +1,59 @@
 from read_aoc_data import read_aoc_data
 import time
 from tqdm import tqdm
+import numpy as np
 
 data = read_aoc_data()
 
-unpacked_data = []
-block = 0
-for idx, value in enumerate(data.strip()):
-    if idx % 2 == 0:
-        unpacked_data.extend([block] * int(value))
-        block += 1
-    else:
-        unpacked_data.extend([None] * int(value))
+# indlæs numpy array
+values = np.array([int(x) for x in data.strip()])
+# opdel i tomme og ikke tomme bloks
+even_indices = np.arange(0, len(values), 2)
+odd_indices = np.arange(1, len(values), 2)
 
+# gentag blocks
+blocks = np.repeat(np.arange(len(even_indices)), values[even_indices])
+
+# lav tomme værdier bruger -1
+none_blocks = np.repeat(-1, np.sum(values[odd_indices]))
+
+# Combine the arrays
+unpacked_data = []
+even_start = 0
+odd_start = 0
+# samler data
+for i in range(len(values)):
+    if i % 2 == 0:
+        count = values[i]
+        unpacked_data.extend(blocks[even_start:even_start + count])
+        even_start += count
+    else:
+        count = values[i]
+        unpacked_data.extend(none_blocks[odd_start:odd_start + count])
+        odd_start += count
+
+unpacked_data = np.array(unpacked_data)
 
 start_time = time.time()
 
-
-def first_none_index(lst: list) -> int:
-    for idx, value in enumerate(lst):
-        if value is None:
-            return idx
-
-
-last_value = None
-for idx in tqdm(range(len(unpacked_data))):
-    first_none = first_none_index(unpacked_data)
-    if first_none:
-        last_value = unpacked_data.pop()
+for _ in tqdm(range(len(unpacked_data))):
+    # tjekker om der er tomme værdier (-1)
+    none_indices = np.where(unpacked_data == -1)[0]
+    if len(none_indices) > 0:
+        first_none = none_indices[0]
+        last_value = unpacked_data[-1]
+        unpacked_data = unpacked_data[:-1]  # fjern sidste værdi
         if first_none < len(unpacked_data):
             unpacked_data[first_none] = last_value
     else:
         break
 
-
-resultat = sum(idx*block for idx, block in enumerate(unpacked_data))
+# finder index af blocks
+indices = np.arange(len(unpacked_data))
+# bruger numpy's array multiplikation
+resultat = np.sum(indices * unpacked_data)
 print(resultat)
 
 end_time = time.time()
-
 print(f"Execution time: {end_time - start_time:.6f} seconds")
+
