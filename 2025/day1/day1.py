9816d33d8a167e61bedaf39bd61ca2b0a0f48ef2
Rud Faden <rudfaden@gmail.com>
2025-12-01 11:52:09 +0100

    dag 1

---
+++ 2025/day1/day1.py
@@ -0,0 +1,43 @@
+dail = tuple(range(0,100))
+
+combination = []
+with open("2025/day1/input.txt") as f:
+    for line in f:     
+        combination.append(line.strip())
+
+turn = 50
+number_zero = 0
+number_crossed_zero = 0
+
+
+for comb in combination:
+    if comb.startswith("L"):
+        move = -int(comb[1:])
+    elif comb.startswith("R"):
+        move = int(comb[1:])
+    else:
+        continue
+    
+    # Tæl hvor mange gange vi krydser 0
+    if move < 0:  # Drej moed ventre
+        # tjek om vi krydser 0 mellem turn og (turn - move)
+        steps = abs(move)
+        for i in range(1, steps + 1):
+            pos = (turn - i) % len(dail)
+            if pos == 0:
+                number_crossed_zero += 1
+    else:  # drej mod højre
+        # tjek om vi krydser 0 mellem turn og (turn + move)
+        steps = move
+        for i in range(1, steps + 1):
+            pos = (turn + i) % len(dail)
+            if pos == 0:
+                number_crossed_zero += 1
+    
+    pos = (turn + move) % len(dail)
+    turn = dail[pos]
+    if turn == 0:
+        number_zero += 1
+    print(f"The dial is rotated {comb} to point at {turn}., during this rotation, it points at 0 {number_crossed_zero} times.")
+
+print(number_zero, number_crossed_zero)
