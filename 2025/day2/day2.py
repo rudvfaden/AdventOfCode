3c70f1def08134cfcf40f3bef07109bf74fed332
Rud Faden <rudfaden@gmail.com>
2025-12-02 17:39:12 +0000

    day222

--- 2025/day2/day2.py
+++ 2025/day2/day2.py
@@ -2,13 +2,40 @@ with open("2025/day2/input.txt", 'r') as file:
     for line in file:
         ranges = line.split(',')
 
+def find_middle(lst):
+    mid_index = len(lst) // 2
+    if len(lst) % 2 == 0:
+        return lst[mid_index - 1:mid_index + 1]
+    else:
+        return lst[mid_index]
+    
+def repated_number(n):
+    s = str(n)
+    if len(s) % 2 == 0:
+        mid = len(s) // 2
+        left = s[:mid]
+        right = s[mid:]
+        if left == right:
+            return int(s)    
+        
+def repated_number_general(n):
+    s = str(n)
+    for k in range(2, find_middle(s)+1):
+        substr = s[:k]
+        mid = substr // 2
+        left = substr[:-mid]
+        right = substr[mid:]
+        if left == right:
+            return int(s[-k:])
 
 
-# rranges = [num.split('-') for num in ranges]
-# r = [list(map(int, sublist)) for sublist in rranges]
+total = 0
+rranges = [num.split('-') for num in ranges]
+r = [list(map(int, sublist)) for sublist in rranges]
 
-# for i in range(r[0][0], r[0][1]+1):
-#         if has_repeated_digits(i):
-#             print(i)
+for rr in rranges:
+    for i in range(int(rr[0]), int(rr[1])+1):
+            if repated_number(i):
+                total += repated_number(i)
 
-print(has_repeated_digits(1188511885))
+print(total)
