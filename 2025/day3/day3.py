97c69ce96e96bbf31a1c77c7c76547d84abef5c2
Rud Faden <rudfaden@gmail.com>
2025-12-03 11:39:44 +0000

    day32

--- 2025/day3/day3.py
+++ 2025/day3/day3.py
@@ -5,12 +5,12 @@ with open('2025/day3/input.txt') as f:
     for line in f:
         l.append(line.strip())
 
-# total = 0
-# for i in l:
-#     set_of_combinations ={int(''.join(pair)) for pair in combinations(i, 2)}
-#     total += max(set_of_combinations)
+total = 0
+for i in l:
+    set_of_combinations ={int(''.join(pair)) for pair in combinations(i, 2)}
+    total += max(set_of_combinations)
 
-# print(total)
+print(total)
 
 
 def max_number_from_digits(s: str, k: int) -> int:
